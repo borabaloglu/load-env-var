29489f8c2a9aecf167a39bbb3194c7f147abcf6b
Bora Baloglu <baloglub@gmail.com>
2020-09-26 19:07:02 +0300

    Implement loadString types

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,6 +4,11 @@ declare interface LoadNumberOptions {
   defaultValue: number;
 }
 
+declare interface LoadStringOptions {
+  defaultValue: number;
+}
+
 declare module "load-env-var" {
   export function loadNumber(name: string, options?: LoadNumberOptions): number;
+  export function loadString(name: string, options?: LoadStringOptions): string;
 }
