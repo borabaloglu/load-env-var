e290db258aba1c6590625fee6f5ec22ebc835831
Bora Baloglu <baloglub@gmail.com>
2020-09-26 20:49:44 +0300

    Implement loadArray types

--- types/index.d.ts
+++ types/index.d.ts
@@ -8,7 +8,15 @@ declare interface LoadStringOptions {
   defaultValue?: string;
 }
 
+declare interface LoadArrayOptions {
+  /* Item types of array elements */
+  type?: "string" | "number";
+  /* Default value is "," */
+  delimiter?: string;
+}
+
 declare module "load-env-var" {
   export function loadNumber(name: string, options?: LoadNumberOptions): number;
   export function loadString(name: string, options?: LoadStringOptions): string;
+  export function loadArray(name: string, options?: LoadArrayOptions): any[];
 }
